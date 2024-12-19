95620795d48ffb710bd35887c02d770ba41507ba
Chichay317 <chic11951@gmail.com>
2024-12-19 03:56:57 +0100

    fix: authentication bug

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-hl^w6g*4&3r_m=b4=qjkw-%xaip0=senn%(s372$_^ml%hvz6m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
